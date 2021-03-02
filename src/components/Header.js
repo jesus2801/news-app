17fd2a11e6f2445d54a4d70b4fa23f8466d9b7a1
jesus2801 <jesusdavid280147@gmail.com>
2021-03-02 13:08:07 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header({title}) {
+  return (
+    <nav className="nav-wrapper light-blue darken-3">
+      <a href="#!" className="brand-logo center">{title}</a>
+    </nav>
+  );
+}
+
+export default Header;
